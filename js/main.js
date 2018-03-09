ec7ba1ed16561ad3690aed6aea32ff268c9485b8
joaopl <contato@impulse4web.com.br>
2018-03-09 01:57:37 -0300

    finish mobile version

--- js/main.js
+++ js/main.js
@@ -1,41 +1,18 @@
 
 jQuery(document).ready(function($) {
+		
+
+	$('.vista_mobile_nav').click(function () {
+	       $('.menu_vista').slideDown( );
+	   });
+	
+	
 
 
 	$(".icon-balanca").click(function (event) {
 	  alert("Você clicou no cabeçalho!");
 	});
 
-	$('.abrir-menu').click(function(event) {
-		event.preventDefault();
-		$('body').css('overflow','hidden');
-		$('#nav').css('display', 'flex');
-		$('#nav').animate({opacity: 1}, 400);
-
-		$(this).animate({
-			opacity: 0,
-		},
-		400, function() {
-			$(this).hide();
-			$('.fechar-menu').show();
-			$('.fechar-menu').animate({opacity: 1}, 400);
-		});
-	});
-
-	$('.fechar-menu').click(function(event) {
-		event.preventDefault();
-		$('body').css('overflow','auto');
-		$('#nav').animate({opacity: 0}, 400, function(){
-			$('#nav').hide();
-		});
-
-		$(this).animate({
-			opacity: 0,
-		},
-		400, function() {
-			$(this).hide();
-			$('.abrir-menu').show();
-			$('.abrir-menu').animate({opacity: 1}, 400);
-		});
-	});
+	
+	
 });
